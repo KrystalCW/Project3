569c149fe408ab372d835c384ba36b07665a9502
pgonzalez003 <pgonzalez003@gmail.com>
2019-06-13 21:21:54 -0700

    Thursday update num. 2

---
+++ test/models/item.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: { type: String, required: true },
+  category: { type: Boolean, required: true },
+  synopsis: String,
+  price: { type: Number, required: true},
+  date: { type: Date, default: Date.now }
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
