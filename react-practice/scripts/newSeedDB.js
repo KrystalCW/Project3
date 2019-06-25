81eb274963cecaaa41d94f1c6720f6436a722b22
Jean-Paul Deshaies <jdeshaies1982@gmail.com>
2019-06-25 00:31:20 -0700

    backend - mongo - newSeedDB file

--- react-practice/scripts/newSeedDB.js
+++ react-practice/scripts/newSeedDB.js
@@ -1,40 +1,9 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
+// This file empties the 'item' collection and inserts the items below
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/itemslist"
-);
-
-/*
-const itemAttachmentSchema = new Schema({
-    file_name: String,
-    file_path: String,
-    file_type: String,
-    file_description: String
-});
-
-const itemSchema = new Schema({
-  item_name: "Leather Couch",
-  item_description: "Description - Leather Couch",
-  item_notes: "Notes - Leather Couch",
-  item_scheduled: false,
-  item_value: "1500",
-  item_purchasePrice: "1500",
-  item_dateObtained: 10/26/2019,
-  item_identificationNumber: "123XYZ",
-  item_idNumberType: "Serial Number",
-  item_quantity: "1",
-  item_locationName: "Living Room",
-  item_locationDescription: "Living Room - Upstairs",
-  item_categoryName: "Furniture",
-  item_categoryDescription: "Home - Office - Furniture",
-  item_attachments: [itemAttachmentSchema]
-});
-
-*/
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/itemslist");
 
 const itemSeed = [
   {
@@ -44,14 +13,28 @@ const itemSeed = [
     item_scheduled: false,
     item_value: "1500",
     item_purchasePrice: "1500",
-    item_dateObtained: 10/26/2019,
+    item_dateObtained: "2019-10-26",
     item_identificationNumber: "123-XYZ",
     item_idNumberType: "Serial Number",
     item_quantity: "1",
     item_locationName: "Living Room",
     item_locationDescription: "Living Room - Upstairs",
     item_categoryName: "Furniture",
-    item_categoryDescription: "Home - Office - Furniture",  
+    item_categoryDescription: "Home - Office - Furniture",
+    item_attachments: [
+        {
+            file_name: "couch_photo.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "Couch Photo"
+        },
+        {
+            file_name: "couch_receipt.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "Couch Receipt"
+        }
+    ]
   },
   {
     item_name: "Piano",
@@ -60,14 +43,28 @@ const itemSeed = [
     item_scheduled: false,
     item_value: "8000",
     item_purchasePrice: "8000",
-    item_dateObtained: 08/13/2008,
+    item_dateObtained: new Date(Date.now()),
     item_identificationNumber: "P-123",
     item_idNumberType: "Serial Number",
     item_quantity: "1",
     item_locationName: "Living Room",
     item_locationDescription: "Living Room - Downstairs",
     item_categoryName: "Musical Instruments",
-    item_categoryDescription: "Musical Instruments",  
+    item_categoryDescription: "Musical Instruments",
+    item_attachments: [
+        {
+            file_name: "piano_photo.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "Piano Photo"
+        },
+        {
+            file_name: "piano_receipt.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "Piano Receipt"
+        }
+    ] 
   },  
   {
     item_name: "Laptop",
@@ -76,7 +73,7 @@ const itemSeed = [
     item_scheduled: false,
     item_value: "750",
     item_purchasePrice: "750",
-    item_dateObtained: 10/26/2019,
+    item_dateObtained: 01/01/2019,
     item_identificationNumber: "LAB-123",
     item_idNumberType: "Serial Number",
     item_quantity: "1",
@@ -84,39 +81,83 @@ const itemSeed = [
     item_locationDescription: "Home Office - Downstairs",
     item_categoryName: "Electronics",
     item_categoryDescription: "Electronics - Home Office",  
+    item_attachments: [
+        {
+            file_name: "laptop_photo.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "Laptop Photo"
+        },
+        {
+            file_name: "laptop_receipt.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "Laptop Receipt"
+        }
+    ]
   },  
   {
-    item_name: "",
-    item_description: "Description - ",
-    item_notes: "Notes - ",
+    item_name: "Gun",
+    item_description: "Description - Gun",
+    item_notes: "Notes - Gun",
     item_scheduled: false,
-    item_value: "",
-    item_purchasePrice: "",
-    item_dateObtained: 10/26/2019,
-    item_identificationNumber: "123XYZ",
+    item_value: "800",
+    item_purchasePrice: "800",
+    item_dateObtained: 07/23/2005,
+    item_identificationNumber: "G-123",
+    item_idNumberType: "Serial Number",
+    item_quantity: "1",
+    item_locationName: "Safe",
+    item_locationDescription: "Safe - Upstairs",
+    item_categoryName: "Sporting Goods",
+    item_categoryDescription: "Hunting - Sporting Goods",
+    item_attachments: [
+        {
+            file_name: "gun_photo.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "Gun Photo"
+        },
+        {
+            file_name: "gun_receipt.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "Gun Receipt"
+        }
+    ]
+  },  
+  {
+    item_name: "TV",
+    item_description: "Description - TV",
+    item_notes: "Notes - TV",
+    item_scheduled: false,
+    item_value: "1500",
+    item_purchasePrice: "1500",
+    item_dateObtained: 02/02/2018,
+    item_identificationNumber: "T-122",
     item_idNumberType: "Serial Number",
     item_quantity: "1",
     item_locationName: "Living Room",
-    item_locationDescription: "Living Room - Downstairs",
-    item_categoryName: "Furniture",
-    item_categoryDescription: "Home - Office - Furniture",  
-  },  
-  {
-    item_name: "",
-    item_description: "Description - ",
-    item_notes: "Notes - ",
-    item_scheduled: false,
-    item_value: "",
-    item_purchasePrice: "",
-    item_dateObtained: 10/26/2019,
-    item_identificationNumber: "123XYZ",
-    item_idNumberType: "Serial Number",
-    item_quantity: "1",
-    item_locationName: "Living Room",
-    item_locationDescription: "Living Room - Downstairs",
-    item_categoryName: "Furniture",
-    item_categoryDescription: "Home - Office - Furniture",  
-  },  
+    item_locationDescription: "Living Room - Upstairs",
+    item_categoryName: "Electronics",
+    item_categoryDescription: "Electronics - Home Office",  
+    item_attachments: [
+        {
+            file_name: "tv_photo.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "TV Photo"
+        },
+        {
+            file_name: "tv_receipt.jpg",
+            file_path: "./resources",
+            file_type: "jpg",
+            file_desciption: "TV Receipt"
+        }
+    ]
+  }
+] 
+
 /*
   {
     item_name: "",
@@ -135,31 +176,7 @@ const itemSeed = [
     item_categoryDescription: "Home - Office - Furniture",  
   },  
 */
-   {
-    name: "Laptop",
-    quantity: 1,
-    scheduled: false,
-    originalPurchaseDate: 01/01/2019,
-    price: 750,
-    date: new Date(Date.now())
-  },
-  {
-    name: "Gun",
-    quantity: 1,
-    scheduled: false,
-    originalPurchaseDate: 07/23/2005,
-    price: 800,
-    date: new Date(Date.now())
-  },
-  {
-    name: "TV",
-    quantity: 1,
-    scheduled: false,
-    originalPurchaseDate: 02/02/2018,
-    price: 1500,
-    date: new Date(Date.now())
-  }
-]
+
 
 db.Item
   .remove({})
