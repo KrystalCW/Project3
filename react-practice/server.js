b71f98e82c034497e206cbfa06280a631e30d4fa
pgonzalez003 <pgonzalez003@gmail.com>
2019-06-27 13:40:42 -0700

    fixed typo

--- react-practice/server.js
+++ react-practice/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 //const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const cors = require("cors");
+
 
 const app = express();
 
