68cc77d07d10348c7f98f9602696a9227b27cd72
Jean-Paul Deshaies <jdeshaies1982@gmail.com>
2019-06-30 03:41:17 -0700

    frontend - react - components - NewUser and RetUser - file index.js - attribute identifier name updates

--- react-practice/client/src/components/NewUser/index.js
+++ react-practice/client/src/components/NewUser/index.js
@@ -48,27 +48,27 @@ class Index extends Component {
                                 <div className="card card-body">
                                     <form>
                                         <div className="form-group">
-                                            <label for="email">Email</label>
+                                            <label htmlFor="email">Email</label>
                                             <input ref={node => this.email = node} type="email" name="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="janedoe1990@aol.com" />
                                             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                                         </div>
                                         <div className="form-group">
-                                            <label for="passwornamed">Name</label>
+                                            <label htmlFor="passwornamed">Name</label>
                                             <input ref={node => this.name = node} type="text" name="name" className="form-control" id="name" placeholder="Jane Doe" />
                                         </div>
                                         <div className="form-group">
-                                            <label for="password">Password</label>
+                                            <label htmlFor="password">Password</label>
                                             <input ref={node => this.password = node} type="password" name="password" className="form-control" id="password" placeholder="Password" />
                                         </div>
                                         <div className="form-group">
-                                                <label for="confirm_password">Confirm Password</label>
+                                                <label htmlFor="confirm_password">Confirm Password</label>
                                                 <input ref={node => this.confirm_password = node} type="password" name="confirm_password" id="confirm_password" className="form-control"  placeholder="Password" />
                                         </div>
                                         <div className="form-group form-check">
                                             <input ref={node => this.toc = node} type="checkbox" className="form-check-input" id="toc" />
-                                            <label className="form-check-label" name="toc" id="toc" for="toc">I Accept the Terms of Service</label>
+                                            <label className="form-check-label" name="toc" id="toc" htmlFor="toc">I Accept the Terms of Service</label>
                                         </div>
-                                        <a href="./items" class="btn btn-info" role="button">Submit</a>
+                                        <a href="./items" className="btn btn-info" role="button">Submit</a>
                                     </form>
                                 </div>
                             </div>
