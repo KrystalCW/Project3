24744ba35862c49b43e6c86ff4ddaf0bacf54767
KrystalCW <krystal.wiederrich@gmail.com>
2019-07-03 01:24:57 -0700

    trying to deploy items

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,21 +4,21 @@ export default {
   // Gets all books
   getItems: function() {
     console.log("got here");
-    return axios.get("/items");
+    return axios.get("/api/items");
   },
   // Gets the book with the given id
   getItem: function(id) {
-    return axios.get("/items/" + id);
+    return axios.get("/api/items/" + id);
   },
   // Deletes the book with the given id
   deleteItem: function(id) {
-    return axios.delete("/items/" + id);
+    return axios.delete("/api/items/" + id);
   },
   // Saves a book to the database
   saveItem: function(itemData) {  
-    return axios.post("/items", itemData);
+    return axios.post("/api/items", itemData);
   },
   updateItem: function(id, itemData) {
-    return axios.put("/items/" + id + "/", itemData)
+    return axios.put("/api/items/" + id + "/", itemData)
   }
 };
