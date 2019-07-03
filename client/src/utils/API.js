8a24679627a141e69700a4201e8ef6dcd4dcf9bc
KrystalCW <krystal.wiederrich@gmail.com>
2019-07-03 01:44:54 -0700

    trying to resolve items page not displaying

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,21 +4,21 @@ export default {
   // Gets all books
   getItems: function() {
     console.log("got here");
-    return axios.get("https://mochifinalproject.herokuapp.com/items");
+    return axios.get("/items");
   },
   // Gets the book with the given id
   getItem: function(id) {
-    return axios.get("https://mochifinalproject.herokuapp.com/items/" + id);
+    return axios.get("/items/" + id);
   },
   // Deletes the book with the given id
   deleteItem: function(id) {
-    return axios.delete("https://mochifinalproject.herokuapp.com/items/" + id);
+    return axios.delete("/items/" + id);
   },
   // Saves a book to the database
   saveItem: function(itemData) {  
-    return axios.post("https://mochifinalproject.herokuapp.com/items", itemData);
+    return axios.post("/items", itemData);
   },
   updateItem: function(id, itemData) {
-    return axios.put("https://mochifinalproject.herokuapp.com/items/" + id + "/", itemData)
+    return axios.put("/items/" + id + "/", itemData)
   }
 };
