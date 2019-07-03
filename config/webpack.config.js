973d02f8ae21b5ae2e79563717c50004d43f3e68
KrystalCW <krystal.wiederrich@gmail.com>
2019-07-02 20:56:06 -0700

    trying to deploy

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,14 +1,14 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, '../client/public');
-var APP_DIR = path.resolve(__dirname, '../client/src/pages');
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-    entry: APP_DIR + '/App.js',
+    entry: APP_DIR + '/index.js',
     output: {
         path: BUILD_DIR,
-        filename: '/bundle.js'
+        filename: 'bundle.js'
     },
     module : {
         loaders : [
