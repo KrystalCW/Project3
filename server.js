34c6668ce4b8d86bfb46d0107ed9c14c1a2adb95
KrystalCW <krystal.wiederrich@gmail.com>
2019-07-02 18:22:38 -0700

    deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require("./config/passport");
 
 
 app.use(logger("dev"));
-app.use(express.static("public"));
+app.use(express.static("./client/public"));
 
 app.use(cors() );
 // Define middleware here
