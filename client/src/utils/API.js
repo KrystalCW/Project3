3526f541bdf5bb4fce6de710206f566d5c89e4ad
KrystalCW <krystal.wiederrich@gmail.com>
2019-07-03 01:34:06 -0700

    trying to get items page to display on Heroku

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,21 +4,21 @@ export default {
   // Gets all books
   getItems: function() {
     console.log("got here");
-    return axios.get("/api/items");
+    return axios.get("https://mochifinalproject.herokuapp.com/items");
   },
   // Gets the book with the given id
   getItem: function(id) {
-    return axios.get("/api/items/" + id);
+    return axios.get("https://mochifinalproject.herokuapp.com/items/" + id);
   },
   // Deletes the book with the given id
   deleteItem: function(id) {
-    return axios.delete("/api/items/" + id);
+    return axios.delete("https://mochifinalproject.herokuapp.com/items/" + id);
   },
   // Saves a book to the database
   saveItem: function(itemData) {  
-    return axios.post("/api/items", itemData);
+    return axios.post("https://mochifinalproject.herokuapp.com/items", itemData);
   },
   updateItem: function(id, itemData) {
-    return axios.put("/api/items/" + id + "/", itemData)
+    return axios.put("https://mochifinalproject.herokuapp.com/items/" + id + "/", itemData)
   }
 };
